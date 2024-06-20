9976dde81db58a9e4fe70ef3935ef220c4eddb7c
Ronak Patel <eronax59@gmail.com>
2024-06-20 19:26:36 +0530

    width issue

--- resources/views/admin/devices/media/index.blade.php
+++ resources/views/admin/devices/media/index.blade.php
@@ -29,7 +29,7 @@
                                 </div>
                                 <div class="flex items-center">
                                     <p class="font-bold p-2"> {{ strtoupper($item->media_type) == 'IMAGE'  ? 'Seconds' : 'Repeat count' }}: </p>
-                                    <p class="w-1">
+                                    <p class="p-1">
                                         <input type="number" id="number-input" aria-describedby="helper-text-explanation" class="repeat-count bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value="{{ $item->repeat_count ?? 1 }}" data-media-id="{{ $item->id }}" required />
                                     </p>
                                 </div>
