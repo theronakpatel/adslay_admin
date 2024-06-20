d6af3e42e0a09c05bf3fac1e93ddcf3599532d25
Ronak Patel <eronax59@gmail.com>
2024-06-21 05:23:39 +0530

    device info

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\VideoController;
 use App\Http\Controllers\Api\DeviceInfoController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 Route::post('/videos', [VideoController::class, 'upload']);
 Route::get('/videos', [VideoController::class, 'index']);
 Route::get('/device-videos/{deviceId}', [VideoController::class, 'getVideos']);
