49a295e749552c7c5c1518c5c9501208523f7cec
Ronak Patel <eronax59@gmail.com>
2024-06-20 19:21:25 +0530

    added position and schedule

--- resources/views/admin/devices/media/index.blade.php
+++ resources/views/admin/devices/media/index.blade.php
@@ -30,7 +30,7 @@
                                 <div class="flex items-center">
                                     <p class="font-bold p-2"> {{ strtoupper($item->media_type) == 'IMAGE'  ? 'Seconds' : 'Repeat count' }}: </p>
                                     <p class="w-1">
-                                        <input type="number" id="number-input" aria-describedby="helper-text-explanation" class="repeat-count bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{ $item->repeat_count ?? 1 }}" data-media-id="{{ $item->id }}" required />
+                                        <input type="number" id="number-input" aria-describedby="helper-text-explanation" class="repeat-count bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value="{{ $item->repeat_count ?? 1 }}" data-media-id="{{ $item->id }}" required />
                                     </p>
                                 </div>
                             </div>
