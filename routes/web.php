ca3e692b1cdddc6b59d146cc3fdd10d360d85987
Ronak Patel <eronax59@gmail.com>
2024-06-22 06:41:16 +0530

    redirection

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function (Request $request) {
+    return redirect('/admin');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::redirect('/dashboard', '/admin');
+
 require __DIR__.'/auth.php';
