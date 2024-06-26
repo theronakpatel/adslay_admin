3ad52cc5cf41815600efeb36947be893aa9a87d6
Ronak Patel <eronax59@gmail.com>
2024-06-26 13:16:18 +0000

    changes

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,14 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/send-test-email', function () {
+    $details = [
+        'title' => 'Mail from Laravel App',
+        'body' => 'This is a test email'
+    ];
 
+    \Mail::to('eronax59@gmail.com')->send(new \App\Mail\TestEmail($details));
+
+    return 'Email sent!';
+});
 require __DIR__.'/auth.php';
